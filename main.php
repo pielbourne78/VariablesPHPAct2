1ffcd83e78ab66c9d9c6ff07578e3e435922baa5
Carl Villafranca <164928549+pielbourne78@users.noreply.github.com>
2025-08-13 01:13:47 +0000

    code initiate

---
+++ main.php
@@ -0,0 +1,15 @@
+<?php
+ $name = "Carl Villafranca";
+    $age = 20;
+    $school = "Global Reciprocal Colleges";
+    $course = "Bachelor of Science in Information Technology";
+    $subject = "WEBSYS";
+    $student = true;
+
+    echo "Name: $name \n";
+    echo "Age: $age\n";
+    echo "School: $school\n";
+    echo "Course: $course\n";
+    echo "Subject: $subject\n";
+    echo "Student: " . ($student ? "Yes" : "No") . "\n";
+?>
